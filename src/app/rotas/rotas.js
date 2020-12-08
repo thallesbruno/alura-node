0d3f6a566d2b165a2ddeb58fac26e3f01ceb1dd5
Thalles Bruno <thallesbgns@gmail.com>
2020-12-08 11:44:00 -0300

    correcoes de edicao

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -57,6 +57,15 @@ module.exports = (app) => {
             .catch(erro => console.log(erro));
     });
 
+    app.put('/livros', function(req, resp) {
+        console.log(req.body);
+
+        const livroDao = new LivroDao(db);
+        livroDao.atualiza(req.body)
+            .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro));
+    });
+
     app.delete('/livros/:id', function(req, resp) {
         const id = req.params.id;
 
